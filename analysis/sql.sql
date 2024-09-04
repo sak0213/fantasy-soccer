709de0d42ac942ea206ca084954f37dbdf4d3955
stekitay1 <steven.kitay0123@gmail.com>
2024-09-04 05:30:10 -0400

    made some new tables

--- analysis/sql.sql
+++ analysis/sql.sql
@@ -1,3 +1,4 @@
+select t.id, t.name, pythag_wl into ffl_prod.team_profiles from (
 select team, sum(score_for) as score_for, sum(score_against) as score_against, sum(score_for)^1.83 / (sum(score_for)^1.83 + sum(score_against)^1.83) as pythag_wl
 from (
 select
@@ -12,5 +13,21 @@ select
 	team_away as team,
 	score_away as score_for,
 	score_home as score_against
-from ffl.fixtures) foo
-group by team
+from ffl.fixtures) a
+group by team) b
+left join ffl.teams as t on t.id = b.team
+
+
+
+select 
+	id,
+	date,
+	case when a.team_home = 40 then a.team_away else a.team_home end as team_away,
+	case when a.team_home = 40 then a.score_home else a.score_away end as score_for,
+	case when a.team_home = 40 then a.score_away else a.score_home end as score_against
+from (
+	select * from ffl.fixtures
+	where season = '2023' and 
+	(team_home = 40 or team_away = 40)) a;
+	
+	select * from ffl_prod.team_profiles where id = 42
