fb05a9727d84d495a30898b668a77a3e321fb710
stekitay1 <steven.kitay0123@gmail.com>
2024-09-09 09:44:03 -0400

    started an approach to model prep

--- analysis/config.py
+++ analysis/config.py
@@ -1,13 +1,3 @@
-from creds import api_key
-
-req_headers = {
-  'x-apisports-key': api_key
-}
-
-base_url = 'https://v3.football.api-sports.io/'
-
-focus_league = 39
-
 # Database config
 local_name = "football"
 local_user = "postgres"
